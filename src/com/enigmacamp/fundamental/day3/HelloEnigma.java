ab6fe9d4dda00714b8aac667d6e55b8923a2f34f
felixa <felixarajiph@gmail.com>
2022-11-04 10:45:43 +0700

    +Added looping for

--- src/com/enigmacamp/fundamental/day3/HelloEnigma.java
+++ src/com/enigmacamp/fundamental/day3/HelloEnigma.java
@@ -3,5 +3,8 @@ package com.enigmacamp.fundamental.day3;
 public class HelloEnigma {
     public static void main(String[] args) {
         System.out.println("Salam enigma");
+        for (int i = 0; i <10 ; i++) {
+            System.out.println("I'm a developer");
+        }
     }
 }
