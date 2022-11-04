49a2540eefdef26fbe7e89d0c6183bd92436e5e6
felixa <felixarajiph@gmail.com>
2022-11-04 10:31:16 +0700

    + add Member

---
+++ src/com/enigmacamp/fundamental/day3/Member.java
@@ -0,0 +1,11 @@
+package com.enigmacamp.fundamental.day3;
+
+public class Member {
+    public static void main(String[] args) {
+     String [][] Members = {
+             {"Jack","1234124"},
+             {"Milla","0298123"},
+             {"Roberto","12134"}
+     }
+    }
+}
